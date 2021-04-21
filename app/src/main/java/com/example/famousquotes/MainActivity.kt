8986e893dd569baf0c76d8d0af6e8c0836474c4f
Nurlibay Koshkinbayev <koshkinbayevn@gmail.com>
2021-04-21 21:51:56 +0500

    Fragments created with jetpack component

--- app/src/main/java/com/example/famousquotes/MainActivity.kt
+++ app/src/main/java/com/example/famousquotes/MainActivity.kt
@@ -8,11 +8,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         navController = findNavController(R.id.fragment)
         setupActionBarWithNavController(navController)
     }
