16f374999c7f438b6197d2ec6116b5c24b42266f
Nurlibay Koshkinbayev <koshkinbayevn@gmail.com>
2021-05-02 21:53:40 +0500

    favorite menu not working !

--- app/src/main/java/com/example/famousquotes/themes/ThemeQuotesAdapter.kt
+++ app/src/main/java/com/example/famousquotes/themes/ThemeQuotesAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.famousquotes.R
-import com.example.famousquotes.data.entities.Citata
 import com.example.famousquotes.data.entities.CitataWithAuthor
 import kotlinx.android.synthetic.main.quotes_item.view.*
 
